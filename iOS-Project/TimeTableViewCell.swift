3a72ca1404ab39fb789d41632fc3d3c52b515175
Bai SA <s@sabais-macbook-pro.local>
2016-05-07 18:33:00 +1000

    CONFILICT

--- iOS-Project/TimeTableViewCell.swift
+++ iOS-Project/TimeTableViewCell.swift
@@ -18,6 +18,8 @@ class TimeTableViewCell: UITableViewCell {
         // Initialization code
     }
     
+    //lIBO
+    
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
         
